67ce895c0fb35dcd13489ae7854d7f2cc73988e2
voidking <voidking@qq.com>
2017-05-12 18:25:21 +0800

    update mapper

---
+++ src/main/java/com/voidking/pandawork/service/UserService.java
@@ -0,0 +1,25 @@
+package com.voidking.pandawork.service;
+
+import com.voidking.pandawork.entity.User;
+
+import java.util.List;
+
+/**
+ * Created by voidking on 2017/5/8.
+ */
+public interface UserService {
+    public void newUser(User user);
+
+    public void delUser(int userId);
+
+    public void updateUser(User user);
+
+    public User queryByUserId(int userId);
+
+    public List<User> listAll();
+
+    public List<User> listByPage(int pageNum, int pageSize);
+
+    public int countUser();
+
+}
---
+++ src/main/java/com/voidking/pandawork/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.voidking.pandawork.service.impl;
+
+import com.voidking.pandawork.entity.User;
+import com.voidking.pandawork.service.UserService;
+
+import java.util.List;
+
+/**
+ * Created by voidking on 2017/5/12.
+ */
+public class UserServiceImpl implements UserService {
+    public void newUser(User user) {
+
+    }
+
+    public void delUser(int userId) {
+
+    }
+
+    public void updateUser(User user) {
+
+    }
+
+    public User queryByUserId(int userId) {
+        return null;
+    }
+
+    public List<User> listAll() {
+        return null;
+    }
+
+    public List<User> listByPage(int pageNum, int pageSize) {
+        return null;
+    }
+
+    public int countUser() {
+        return 0;
+    }
+}
